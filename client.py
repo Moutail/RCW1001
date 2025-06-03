6bc4525effd543a6b6586414305b4e65648f25b5
cheriff <128246394+Moutail@users.noreply.github.com>
2025-06-03 10:31:44 -0400

    update

--- client.py
+++ client.py
@@ -1,5 +1,13 @@
 import requests
-url = "https://rcw1001-dhdjeqb7cyfvhkh8.canadacentral-01.azurewebsites.net/test"
+# url = "http://localhost:8000/test"
+url = 'https://rcw1001-dhdjeqb7cyfvhkh8.canadacentral-01.azurewebsites.net/'
+ 
 response = requests.get(url)
-response = response.json()
-print(response['message'])
+ 
+try:
+    response = response.json()
+    print(response['message'])
+   
+ 
+except Exception as e:
+    print("Error: Response is not in JSON format",e)
